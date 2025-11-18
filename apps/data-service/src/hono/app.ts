9a343e45f362f8504b39fd678380482904b3ef71
Dieguinho Pinho <29768792+diegocuehdz@users.noreply.github.com>
2025-11-18 03:58:04 -0600

    feat: add georouting and api route

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,11 +1,23 @@
 import { Hono } from 'hono';
 
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
 import { getLink } from '@repo/data-ops/queries/links';
+
+import { getDestinationForCountry } from '@/helpers/route-ops';
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
 	const id = c.req.param('id');
+
 	const linkFromDb = await getLink(id)
-	return c.json(linkFromDb)
+	if (!linkFromDb) return c.text('Destination link not found', 404)
+
+	const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf)
+	if (!cfHeader.success) return c.text('Invalid Cloudflare headers', 400)
+
+	const headers = cfHeader.data;
+	const destination = getDestinationForCountry(linkFromDb, headers.country)
+
+	return c.redirect(destination);
 })
