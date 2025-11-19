080e90d2f032e5913e68d1b635163d3adf1b67be
Dieguinho Pinho <29768792+diegocuehdz@users.noreply.github.com>
2025-11-19 02:08:11 -0600

    feat: add KV binding ad cache logic

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,16 +1,15 @@
 import { Hono } from 'hono';
 
 import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
-import { getLink } from '@repo/data-ops/queries/links';
 
-import { getDestinationForCountry } from '@/helpers/route-ops';
+import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
 	const id = c.req.param('id');
 
-	const linkFromDb = await getLink(id)
+	const linkFromDb = await getRoutingDestinations(c.env, id)
 	if (!linkFromDb) return c.text('Destination link not found', 404)
 
 	const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf)
