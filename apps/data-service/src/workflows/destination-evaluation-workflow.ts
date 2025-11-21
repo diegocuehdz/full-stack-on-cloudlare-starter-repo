8985539ad5b341b7be4a5b46e0aeb37623b6061f
Dieguinho Pinho <29768792+diegocuehdz@users.noreply.github.com>
2025-11-21 02:23:26 -0600

    fix: initDB at workflow level

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -4,10 +4,14 @@ import { addEvaluation } from '@repo/data-ops/queries/evaluations';
 
 import { collectDestinationInfo } from '@/helpers/browser-render';
 import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
+import { initDatabase } from '@repo/data-ops/database';
 
 export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
 
 	async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
+
+		initDatabase(this.env.DB);
+
 		const collectedData = await step.do("Collect rendered destination page data", async () => {
 			return collectDestinationInfo(this.env, event.payload.destinationUrl);
 		});
